d86444240e985ec123baba1780b5e311d7f27fab
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2022-11-15 17:33:37 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+body.style
+
+const startColor = () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+if (timerId !== null) {
+    
+}
+
+const stopColor = () => {
+  clearInterval(timerId);
+};
+
+btnStart.addEventListener('click', startColor);
+btnStop.addEventListener('click', stopColor);
