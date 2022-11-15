dcb3e69e6b5b59aa297f198470cd528564b8a00d
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2022-11-15 17:42:31 +0100

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,20 +6,21 @@ const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 let timerId = null;
-body.style
+body.style;
 
 const startColor = () => {
   timerId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
-};
 
-if (timerId !== null) {
-    
-}
+  if (timerId !== null) {
+    btnStart.disabled = true;
+  }
+};
 
 const stopColor = () => {
   clearInterval(timerId);
+  btnStart.disabled = false;
 };
 
 btnStart.addEventListener('click', startColor);
