197f753af886f8cfa0d4bbf9beb4729ed93af644
SzEmil <106548124+SzEmil@users.noreply.github.com>
2022-11-15 16:34:06 +0000

    Deploying to gh-pages from @ SzEmil/goit-js-hw-09@d86444240e985ec123baba1780b5e311d7f27fab 🚀

---
+++ 01-color-switcher.5ca2c68e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;n.style;t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.5ca2c68e.js.map
